37939ae2635caa56162df606ecf81ce0ab297f4e
Akshat Mittal <akshatmittal.0007@gmail.com>
2023-03-03 21:39:39 +0530

    minor change to make helix scroller work

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
       <div
         className="pannelContainer"
         style={{
-          height: "50vh",
+          height: "100vh",
         }}
       ></div>
       <ImageHelixScroller />
